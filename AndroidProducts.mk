e853f70ca8ae548dd08671ce8f8bf414f7438760
Cheewaca <42944307+cheewaca@users.noreply.github.com>
2020-04-19 23:14:11 +0200

    Update: AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,10 +14,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h870.mk
+    $(LOCAL_DIR)/stag_h870.mk
 
 	
 COMMON_LUNCH_CHOICES := \
-    lineage_h870-user \
-    lineage_h870-userdebug \
-    lineage_h870-eng
+    stag_h870-user \
+    stag_h870-userdebug \
+    stag_h870-eng
